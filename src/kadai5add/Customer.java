6e2f3658b9bf4b562744303542536b66e239cd87
y-kawashima <y-kawashima@ASP002304-109193MP>
2024-04-30 11:40:55 +0900

    Merge branch 'master' of https://github.com/147963258to/canon_git.git

--- src/kadai5add/Customer.java
+++ src/kadai5add/Customer.java
@@ -1,59 +1,42 @@
 package kadai5add;
 
 public class Customer {
-	//フィールド（メンバ変数）の定義
-		String name; //名前
-		int id;//ID
-		
-	//コンストラクタの定義
-		public Customer() {
+	//フィールド
+	private int id;      //ID
+	private String name; //名前
+	
+	//コンストラクタ
+	public Customer() {
 		
 	}
 	
-		public Customer(String name) {
+	public Customer(String name) {
 		this.name = name;
-		
 	}
 	
-		public Customer(int id,String name) {
-			this.name = name;
-			this.id = id;
-		}
-		
-		/**
-		 * @return name
-		 */
-		public String getName() {
-			return name;
-		}
-
-		/**
-		 * @param name セットする name
-		 */
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		/**
-		 * @return id
-		 */
-		public int getId() {
-			return id;
-		}
-
-		/**
-		 * @param id セットする id
-		 */
-		public void setId(int id) {
-			this.id = id;
-		}
-
-		public void showData() {
-			System.out.println("ID：" + id);
-			System.out.println("名前：" + name);
-			
-		}
+	public Customer(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 	
-	
-	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	//showDataメソッド
+	public void showData() {
+		System.out.println(id + "：" + name);
+	}
 }
--- src/kadai5add/UseCustomer2.java
+++ src/kadai5add/UseCustomer2.java
@@ -3,90 +3,91 @@ package kadai5add;
 import java.util.Scanner;
 
 public class UseCustomer2 {
+	static Scanner scan;
+
 	public static void main(String[] args) {
 		Customer[] customerList = new Customer[3];
-		Scanner scan = new Scanner(System.in);
-		// 顧客データの読み込み
+		scan = new Scanner(System.in);
+
+		// 顧客データの読み込み 
 		for (int index = 0; index < customerList.length; index++) {
-			System.out.println((index + 1)
-					+ "人目の顧客情報を入力してください。");
+			System.out.println((index + 1) + "人目の顧客情報を入力してください。");
 			System.out.print("ID：");
-			int id = scan.nextInt(); // IDの入力
-			System.out.print("名前：");
-			String name = scan.next(); // 名前の入力
+			int id = scan.nextInt(); // IDの入力 
 			
+			System.out.print("名前：");
+			String name = scan.next(); // 名前の入力 
+
 			Customer customer;
 			if (id == -1) {
-				// IDが-1なら名前だけ登録
+				// IDが-1なら名前だけ登録 
 				customer = new Customer(name);
 			} else {
-				// IDが-1でなければIDと名前を登録
+				// IDが-1でなければIDと名前を登録 
 				customer = new Customer(id, name);
 			}
 			customerList[index] = customer;
-
 		}
+		
+		int num;
 
-		System.out.println();
-		int ctrl = 0;
-		while (ctrl != 9) {
-
+		do {
 			System.out.print("操作（1:一覧表示 2:変更 9:終了）を選択してください：");
-			ctrl = scan.nextInt(); // 操作入力
-
-			switch (ctrl) {
+			num = scan.nextInt();
+			
+			switch (num) {
 			case 1:
-				showCustomerList(customerList);
+				showList(customerList);
 				break;
 			case 2:
-				changeId(customerList);
+				updateRecord(customerList);
 				break;
-			case 9:
-				scan.close();
-				break;
-
-			default:
-				//	throw new IllegalArgumentException("Unexpected value: " + ctrl);
 			}
-			System.out.println();
-		}
+			
+			
+		} while(num != 9);
+		
 		scan.close();
 	}
-	//メソッドの定義
-	public static void showCustomerList(Customer[] customerList) {
-		// 顧客データ表示
+
+	static void showList(Customer[] customerList) {
+		// 顧客データ表示 
+		System.out.println();
 		System.out.println("=== 顧客データ一覧 ===");
+		
 		for (Customer customer : customerList) {
 			customer.showData();
-		}
-
+		}	
 	}
 
-	
-	public static void changeId(Customer[] customerList) {
-		Scanner scan = new Scanner(System.in);
+	static void updateRecord(Customer[] customerList) {
+		// 顧客データ表示 
+		System.out.println();
 		System.out.print("更新するID：");
-		int oldId = scan.nextInt(); // 操作入力
-
-		boolean error = true;
+		int id = scan.nextInt();
+		
+		Customer hit = null;
+		
 		for (Customer customer : customerList) {
-
-			if (customer.id == oldId) {
-
-				System.out.print("新しいID：");
-				int newId = scan.nextInt(); // 操作入力
-				System.out.print("新しい名前：");
-				String newName = scan.next(); // 操作入力
-				customer.id = newId;
-				customer.name = newName;
-				error = false;
-
+			if (id == customer.getId()) {
+				hit = customer;
 				break;
 			}
 		}
-		if (error) {
+		
+		if (hit == null) {
 			System.out.println("指定したIDは存在しません。");
+			return;
 		}
-
+		
+		System.out.println("更新するID：" + id);
+		System.out.print("新しいID：");
+		int newId = scan.nextInt();
+		
+		System.out.print("新しい名前：");
+		String newName = scan.next();
+		
+		hit.setId(newId);
+		hit.setName(newName);
 	}
 }
